5e9409b199f384b6884e17914da8353fb1dde872
Xervanik <john.pettit@cox.net>
2015-02-15 10:00:12 -0800

    Added Account

--- testone/Scripts/testcubewars/testcubewars/urls.py
+++ testone/Scripts/testcubewars/testcubewars/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from mycubewars.models import Account
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 
@@ -9,14 +10,24 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         model = User
         fields = ('url', 'username', 'email', 'is_staff')
 
+class AccountSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Account
+        fields = ('username', 'password', 'create_date')
+
 # ViewSets define the view behavior.
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'accounts', AccountViewSet)
 
 urlpatterns = patterns('',
     # Examples:
